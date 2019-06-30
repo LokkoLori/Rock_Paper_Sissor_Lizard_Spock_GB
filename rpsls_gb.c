60ab776fc80bd3fe48be8304e989466cbb39ed96
Kaptas Andras <kaptas.andras@gmail.com>
2019-06-30 15:05:46 +0200

    graphics experiments, aritmetic WTFs in sprinte moving basics hace broken the developing process

--- rpsls_gb.c
+++ rpsls_gb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <rand.h>
 #include <gb/gb.h>
+#include "rpsls_map.c"
 
 #define RSPLS_ROCK 0
 #define RSPLS_PAPER 1
@@ -31,17 +32,20 @@ const char* const RSPLS_BEATS[25] = {
 	"vaporises", "", "smashes", "", ""
 };
 
-int player_num = 1;
-int opponent_choise = RSPLS_UNDEF;
-int my_choise = RSPLS_ROCK;
-int game_phase = PH_MATCHMAKING;
-int i = 0;
-int j = 0;
-int d = 0;
-int seed = 0;
-int seedinited = 0;
-int points_mine = 0;
-int points_his = 0;
+INT16 player_num = 1;
+INT16 opponent_choise = RSPLS_UNDEF;
+INT16 my_choise = RSPLS_ROCK;
+INT16 game_phase = PH_MATCHMAKING;
+INT16 i = 0;
+INT16 j = 0;
+INT16 d = 0;
+INT16 seed = 0;
+INT16 seedinited = 0;
+INT16 points_mine = 0;
+INT16 points_his = 0;
+
+INT16 ramapos_x = 0;
+INT16 ramapos_y = 0;
 
 void matchmaking(){
 	
@@ -175,8 +179,108 @@ void matching(){
 	game_phase = PH_CHOOSING;
 }
 
+void set_rama_sprite(){
+	
+	SPRITES_8x8;
+	
+	set_sprite_data(0, 88, RPSLS_MAP);
+	
+	set_sprite_tile(0, 80);
+	set_sprite_tile(1, 81);
+	set_sprite_tile(2, 81);
+	set_sprite_tile(3, 82);
+	set_sprite_tile(4, 83);
+	set_sprite_tile(5, 83);
+	set_sprite_tile(6, 84);
+	set_sprite_tile(7, 85);
+	set_sprite_tile(8, 85);
+	set_sprite_tile(9, 86);
+	set_sprite_tile(10, 87);
+	set_sprite_tile(11, 87);
+}
+
+void show_rame_sprite_at(INT16 x, INT16 y) {
+
+	move_sprite(0, x, y);
+	move_sprite(1, x+8, y);
+	move_sprite(2, x+16, y);
+	move_sprite(3, x+24, y);
+	move_sprite(4, x+24, y+8);
+	move_sprite(5, x+24, y+16);
+	move_sprite(6, x+24, y+24);
+	move_sprite(7, x+16, y+24);
+	move_sprite(8, x+8, y+24);
+	move_sprite(9, x, y+24);
+	move_sprite(10, x, y+16);
+	move_sprite(11, x, y+8);
+	
+	
+	SHOW_SPRITES;
+}
+
+void send_rama_to(INT16 x, INT16 y, INT16 in_step) {
+	
+	// this function is a pile of dogshit!!!
+	
+	INT16 x_count = x - ramapos_x;
+	INT16 y_count = y - ramapos_y;
+	
+	INT16 x_step = in_step / x_count;
+	INT16 y_step = in_step / y_count;
+	
+	printf("%d %d %d\n", y, ramapos_y, y_count);
+	
+	x_count = 0;
+	y_count = 0;
+	
+	for(i=0;i<in_step;i++){
+		if (x_step < 0)
+			x_count--;
+		else
+			x_count++;
+		if (x_count == x_step) {
+			x_count = 0;
+			if (x_step < 0)
+				ramapos_x--;
+			else
+				ramapos_x++;
+			show_rame_sprite_at(ramapos_x, ramapos_y); 
+		}
+		
+		if (y_step < 0)
+			y_count--;
+		else
+			y_count++;
+		if (y_count == y_step) {
+			y_count = 0;
+			if (y_step < 0)
+				ramapos_y--;
+			else
+				ramapos_y++;
+			show_rame_sprite_at(ramapos_x, ramapos_y); 
+		}
+	}
+	
+	ramapos_x = x;
+	ramapos_y = y;
+	
+	show_rame_sprite_at(ramapos_x, ramapos_y); 
+}
 
 void main(void) {
+	
+	INT16 a = 0;
+	printf("%d\n", a);
+	a = a - 90;
+	printf("%d\n", a);
+	
+	set_rama_sprite();
+	send_rama_to(45, 90, 10000);
+	
+	send_rama_to(90, 0, 10000);
+	
+	while(1)
+		seed++;
 	
 	while (1) {
 		

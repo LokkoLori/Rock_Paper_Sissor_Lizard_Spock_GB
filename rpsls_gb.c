7e7c7ee33d72a98bfc72347a986735d4e9c918d5
Kaptas Andras <kaptas.andras@gmail.com>
2019-06-30 17:07:49 +0200

    change to gbdk-2.95-3-win32 to solve compile anomalies (and get new ones) select frame moving around the screen

--- rpsls_gb.c
+++ rpsls_gb.c
@@ -24,6 +24,10 @@ const char* const RSPLS_NAMES[5] = {
 	"SPOCK"
 };
 
+const INT16 const RSPLS_POSITIONS[10] = {
+	5, 3, 11, 3, 13, 8, 8, 10, 3, 8
+};
+
 const char* const RSPLS_BEATS[25] = {
 	"", "", "smashes", "crushes", "",
 	"covers", "", "", "", "disprooves",
@@ -46,6 +50,95 @@ INT16 points_his = 0;
 
 INT16 ramapos_x = 0;
 INT16 ramapos_y = 0;
+
+void set_rama_sprite(){
+	
+	SPRITES_8x8;
+	
+	set_sprite_data(0, 88, RPSLS_MAP);
+	
+	set_sprite_tile(0, 80);
+	set_sprite_tile(1, 81);
+	set_sprite_tile(2, 81);
+	set_sprite_tile(3, 82);
+	set_sprite_tile(4, 83);
+	set_sprite_tile(5, 83);
+	set_sprite_tile(6, 84);
+	set_sprite_tile(7, 85);
+	set_sprite_tile(8, 85);
+	set_sprite_tile(9, 86);
+	set_sprite_tile(10, 87);
+	set_sprite_tile(11, 87);
+}
+
+void show_rame_sprite_at(INT16 x, INT16 y) {
+
+	x = x + 8;
+	y = y + 16;
+	
+	move_sprite(0, x, y);
+	move_sprite(1, x+8, y);
+	move_sprite(2, x+16, y);
+	move_sprite(3, x+24, y);
+	move_sprite(4, x+24, y+8);
+	move_sprite(5, x+24, y+16);
+	move_sprite(6, x+24, y+24);
+	move_sprite(7, x+16, y+24);
+	move_sprite(8, x+8, y+24);
+	move_sprite(9, x, y+24);
+	move_sprite(10, x, y+16);
+	move_sprite(11, x, y+8);
+	
+	SHOW_SPRITES;
+}
+
+void send_rama_to(INT16 x, INT16 y, INT16 in_step) {
+	
+	// this function is a pile of dogshit!!!
+	
+	INT16 x_count = x - ramapos_x;
+	INT16 y_count = y - ramapos_y;
+	
+	INT16 x_step = in_step / x_count;
+	INT16 y_step = in_step / y_count;
+	
+	x_count = 0;
+	y_count = 0;
+	
+	for(i=0;i<in_step;i++){
+		if (x_step < 0)
+			x_count--;
+		else
+			x_count++;
+		if (x_count == x_step) {
+			x_count = 0;
+			if (x_step < 0)
+				ramapos_x--;
+			else
+				ramapos_x++;
+			show_rame_sprite_at(ramapos_x, ramapos_y); 
+		}
+		
+		if (y_step < 0)
+			y_count--;
+		else
+			y_count++;
+		if (y_count == y_step) {
+			y_count = 0;
+			if (y_step < 0)
+				ramapos_y--;
+			else
+				ramapos_y++;
+			show_rame_sprite_at(ramapos_x, ramapos_y); 
+		}
+	}
+	
+	ramapos_x = x;
+	ramapos_y = y;
+	
+	show_rame_sprite_at(ramapos_x, ramapos_y); 
+	
+}
 
 void matchmaking(){
 	
@@ -82,17 +175,21 @@ void matchmaking(){
 }
 
 void selection_changed(){
-	if (my_choise < 0){
+	if (my_choise == -1){
 		my_choise = 4;
 	}
-	if (4 < my_choise){
+	if (my_choise == 5){
 		my_choise = 0;
 	}
 	printf("Select: %d %s\n", my_choise, RSPLS_NAMES[my_choise]);
+	
+	send_rama_to(RSPLS_POSITIONS[my_choise*2]*8,RSPLS_POSITIONS[my_choise*2+1]*8, 1000);
 }
 	
 
 void choosing(){
+	
+	set_rama_sprite();
 	
 	if (player_num == 2) {
 		receive_byte();
@@ -100,8 +197,9 @@ void choosing(){
 	
 	selection_changed();
 	while(1){
-		if (!seedinited)
+		if (!seedinited){
 			seed++;
+		}
 		if (joypad() == J_RIGHT || joypad() == J_DOWN) {
 			waitpadup();
 			my_choise++;
@@ -119,8 +217,9 @@ void choosing(){
 		}
 	}
 	
+	
 	if (!seedinited){
-		initarand(seed);
+		initrand(seed);
 		//printf("rand inited with seed: %d\n", seed);
 		seedinited = 1;
 	}
@@ -179,110 +278,12 @@ void matching(){
 	game_phase = PH_CHOOSING;
 }
 
-void set_rama_sprite(){
-	
-	SPRITES_8x8;
-	
-	set_sprite_data(0, 88, RPSLS_MAP);
-	
-	set_sprite_tile(0, 80);
-	set_sprite_tile(1, 81);
-	set_sprite_tile(2, 81);
-	set_sprite_tile(3, 82);
-	set_sprite_tile(4, 83);
-	set_sprite_tile(5, 83);
-	set_sprite_tile(6, 84);
-	set_sprite_tile(7, 85);
-	set_sprite_tile(8, 85);
-	set_sprite_tile(9, 86);
-	set_sprite_tile(10, 87);
-	set_sprite_tile(11, 87);
-}
-
-void show_rame_sprite_at(INT16 x, INT16 y) {
-
-	move_sprite(0, x, y);
-	move_sprite(1, x+8, y);
-	move_sprite(2, x+16, y);
-	move_sprite(3, x+24, y);
-	move_sprite(4, x+24, y+8);
-	move_sprite(5, x+24, y+16);
-	move_sprite(6, x+24, y+24);
-	move_sprite(7, x+16, y+24);
-	move_sprite(8, x+8, y+24);
-	move_sprite(9, x, y+24);
-	move_sprite(10, x, y+16);
-	move_sprite(11, x, y+8);
-	
-	
-	SHOW_SPRITES;
-}
-
-void send_rama_to(INT16 x, INT16 y, INT16 in_step) {
-	
-	// this function is a pile of dogshit!!!
-	
-	INT16 x_count = x - ramapos_x;
-	INT16 y_count = y - ramapos_y;
-	
-	INT16 x_step = in_step / x_count;
-	INT16 y_step = in_step / y_count;
-	
-	printf("%d %d %d\n", y, ramapos_y, y_count);
-	
-	x_count = 0;
-	y_count = 0;
-	
-	for(i=0;i<in_step;i++){
-		if (x_step < 0)
-			x_count--;
-		else
-			x_count++;
-		if (x_count == x_step) {
-			x_count = 0;
-			if (x_step < 0)
-				ramapos_x--;
-			else
-				ramapos_x++;
-			show_rame_sprite_at(ramapos_x, ramapos_y); 
-		}
-		
-		if (y_step < 0)
-			y_count--;
-		else
-			y_count++;
-		if (y_count == y_step) {
-			y_count = 0;
-			if (y_step < 0)
-				ramapos_y--;
-			else
-				ramapos_y++;
-			show_rame_sprite_at(ramapos_x, ramapos_y); 
-		}
-	}
-	
-	ramapos_x = x;
-	ramapos_y = y;
-	
-	show_rame_sprite_at(ramapos_x, ramapos_y); 
-}
 
 void main(void) {
-	
-	INT16 a = 0;
-	printf("%d\n", a);
-	a = a - 90;
-	printf("%d\n", a);
-	
-	set_rama_sprite();
-	send_rama_to(45, 90, 10000);
-	
-	send_rama_to(90, 0, 10000);
-	
-	while(1)
-		seed++;
+
 	
 	while (1) {
+		
 		
 		switch (game_phase) {
 			case PH_MATCHMAKING:
